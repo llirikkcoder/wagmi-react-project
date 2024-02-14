1e0c6ff063e9b849533763ebc39b33ad971b7345
Kirill Pavliashik <kpavliashik@netevia.com>
2024-02-14 17:06:32 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import { useAccount, useConnect, useDisconnect } from 'wagmi'
+import { Account } from './components/Account'
+import { Balance } from './components/Balance'
+
+function App() {
+  const account = useAccount()
+  const { connectors, connect, status, error } = useConnect()
+  const { disconnect } = useDisconnect()
+
+  return (
+    <>
+      <div>
+        <h2>Account</h2>
+
+        <div>
+          status: {account.status}
+          <br />
+          addresses: {JSON.stringify(account.addresses)}
+          <br />
+          chainId: {account.chainId}
+        </div>
+
+        {account.status === 'connected' && (
+          <button type="button" onClick={() => disconnect()}>
+            Disconnect
+          </button>
+        )}
+      </div>
+
+      <div>
+        <h2>Connect</h2>
+        {connectors.map((connector) => (
+          <button
+            key={connector.uid}
+            onClick={() => connect({ connector })}
+            type="button"
+          >
+            {connector.name}
+          </button>
+        ))}
+        <div>{status}</div>
+        <div>{error?.message}</div>
+      </div>
+
+      <br />
+      <hr />
+      <h2>Account</h2>
+      <Account />
+      <br />
+      <hr />
+      <h2>Balance</h2>
+      <Balance />
+    </>
+  )
+}
+
+export default App
